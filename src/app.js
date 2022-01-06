35432e3b8db14693a84fc2a4521a4ce795a0749e
Cleiton1912 <cleiton.silvalima@outlook.fr>
2022-01-06 21:29:26 +0100

    1st modifs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const App = () => {
-    return (<h1 className="app">Say hello to ReactJS!</h1>); 
+    return (
+    <div>
+        <h1 className="app">Say hello to ReactJS!</h1>
+        <div>
+            <p>You will learn how to use the most popular frontend libary, and become a super Ninja developer.</p>
+        </div>
+        <button>Awesome!</button>
+        <div>
+            <img src="/images/icon1.png" alt="cle"></img>
+            <h3>Declaractive</h3>
+            <p>React makes it painless to create interactive Uls.</p>
+        </div>
+        <div>
+        <img src="/images/icon2.png" alt="play"></img>
+        <h3>Components</h3>
+        <p>Build encaspsulated components that manage their state.</p>
+        </div>
+        <div>
+        <img src="/images/icon3.png" alt="settings"></img>
+        <h3>Single-Way</h3>
+        <p>A set of immutable values are passed to the component's</p>
+        </div>
+        <div>
+        <img src="/images/icon4.png" alt="ordi"></img>
+        <p>Statically-typed, designed to run on modern browsers.</p>
+        <h3>JSX</h3>
+        </div>
+    </div>
+    
+    
+    )
 }
 
 export default App;
