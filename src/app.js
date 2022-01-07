89cdab894ca80f397666080f298c2366d97bfc44
Claire Pottier <claire_pottier@hotmail.fr>
2022-01-07 23:15:14 +0100

    styling - navbar icons to do

--- src/app.js
+++ src/app.js
@@ -3,15 +3,20 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <div class="container">
-        <h1 className="app">Say hello to ReactJS!</h1>
-        <p>
-          You will learn how to use the most popular frontend libary, and become
-          a super Ninja developer.
-        </p>
-        <button>Awesome!</button>
-      </div>
-      <div class="box">
+      <section id="container">
+        <div id="nav">
+          <img class="ironhack-logo" src="/images/ironhack-logo.svg" alt="Ironhack logo"></img>
+          <img class="menu-icon" src="/images/menu-top.svg" alt="Menu icon"></img>
+        </div>
+        <div id="container-txt">
+          <h1>Say hello to ReactJS</h1>
+          <p>You will learn how to use</p>
+          <p>the most popular frontend libary,</p>
+          <p>and become a super Ninja developer.</p>
+          <button>Awesome!</button>
+        </div>
+      </section>
+      <section id="box">
         <div>
           <img class="img" src="/images/icon1.png" alt="cle"></img>
           <h3>Declaractive</h3>
@@ -32,7 +37,7 @@ const App = () => {
           <h3>JSX</h3>
           <p>Statically-typed, designed to run on modern browsers.</p>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
